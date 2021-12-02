232b6a1e7f7bc2432c4195548978edf76148cd09
shakib907 <sakibhasan907@gmail.com>
2021-12-02 20:34:48 +0600

    first commit

---
+++ php.php
@@ -0,0 +1,19 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','form');
+
+?>
+<?php
+
+print_r($_POST);
+?>
+<?php
+ <form action="" method="post">
+echo <p>fname <input type="text" name="fname"></p>
+echo <p>lname <input type="text" name="lname"></p>
+echo <input type="submit">
+
+
+</form>
+
+?>
